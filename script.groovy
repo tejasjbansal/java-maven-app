16da331e9d4b78c8d0c7506361b129e481264a5f
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2023-02-19 12:18:59 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Docker-Id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t tejashbansal/my-repo:jma-2.0 .'
-        sh "docker login -u $USER -p $PASS"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push tejashbansal/my-repo:jma-2.0'
     }
 } 
