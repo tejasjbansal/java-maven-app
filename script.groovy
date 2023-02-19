6b668e7e8bf19c04149527925dcdac01329142d3
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2023-02-19 12:13:53 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'Docker-Id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tejashbansal/my-repo:jma-2.0 .'
+        sh "docker login -u $USER -p $PASS"
+        sh 'docker push tejashbansal/my-repo:jma-2.0'
     }
 } 
 
