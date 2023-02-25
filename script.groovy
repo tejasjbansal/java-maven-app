617b3fd18a1d426729ee2180d429b461bc8e18d5
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2023-02-25 15:09:42 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Docker-Id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t tejashbansal/my-repo:$IMAGE_NAME ."
+        sh "docker build -t tejashbansal/my-repo:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push tejashbansal/my-repo:$IMAGE_NAME"
+        sh "docker push tejashbansal/my-repo:${IMAGE_NAME}"
     }
 } 
 
